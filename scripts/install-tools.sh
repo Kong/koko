5dee6ba307837b9603075366531620e6aa69824a
Harry Bagdi <harrybagdi@gmail.com>
2021-10-27 17:12:11 -0700

    chore: speed up install script and check for docker

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -8,6 +8,14 @@ then
   exit 1
 fi
 
+# check docker installed
+docker version > /dev/null 2>&1
+if [[ $? -ne 0 ]];
+then
+  echo "Please install docker on the system."
+  :wqa
+  exit 1
+fi
 
 go install "github.com/bufbuild/buf/cmd/buf"
 go install "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
@@ -16,6 +24,13 @@ go install "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 go install "github.com/kong/go-wrpc/cmd/protoc-gen-go-wrpc"
 go install "google.golang.org/protobuf/cmd/protoc-gen-go"
 
-curl -sSfL \
-"https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh" \
-| sh -s -- -b bin v1.42.1
+# install golangci-lint if needed
+
+GOLANGCI_LINT_VERSION=1.42.1
+FOUND_LINT_VERSION=$(./bin/golangci-lint version --format short)
+if [[ "$FOUND_LINT_VERSION" != "${GOLANGCI_LINT_VERSION}" ]];
+then
+  curl -sSfL \
+  "https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh" \
+  | sh -s -- -b bin v${GOLANGCI_LINT_VERSION}
+fi
