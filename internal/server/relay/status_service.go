fa9214b017e40f391ce496588a2c9936d8feda94
Harry Bagdi <harrybagdi@gmail.com>
2022-01-27 14:29:16 -0800

    feat: implement status update service in relay server

---
+++ internal/server/relay/status_service.go
@@ -0,0 +1,149 @@
+package relay
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/google/uuid"
+	adminModel "github.com/kong/koko/internal/gen/grpc/kong/admin/model/v1"
+	relay "github.com/kong/koko/internal/gen/grpc/kong/relay/service/v1"
+	"github.com/kong/koko/internal/model"
+	"github.com/kong/koko/internal/resource"
+	"github.com/kong/koko/internal/server/util"
+	"github.com/kong/koko/internal/store"
+	"go.uber.org/zap"
+	"google.golang.org/grpc/status"
+)
+
+type StatusServiceOpts struct {
+	StoreLoader util.StoreLoader
+	Logger      *zap.Logger
+}
+
+func NewStatusService(opts StatusServiceOpts) *StatusService {
+	res := &StatusService{
+		storeLoader: opts.StoreLoader,
+		logger:      opts.Logger,
+	}
+	return res
+}
+
+type StatusService struct {
+	relay.UnimplementedStatusServiceServer
+	storeLoader util.StoreLoader
+	logger      *zap.Logger
+}
+
+func (s StatusService) UpdateStatus(ctx context.Context,
+	req *relay.UpdateStatusRequest) (*relay.UpdateStatusResponse, error) {
+	err := validateRef(req.Item.ContextReference)
+	if err != nil {
+		return nil, util.ErrClient{Message: err.Error()}
+	}
+	db, err := s.getDB(ctx, req.Cluster)
+	if err != nil {
+		return nil, err
+	}
+
+	res := resource.NewStatus()
+	res.Status = req.Item
+
+	refType := req.Item.ContextReference.Type
+	refID := req.Item.ContextReference.Id
+	id, err := s.getCurrentStatusID(ctx, db, refType, refID)
+	if err != nil {
+		return nil, err
+	}
+	if id != "" {
+		res.Status.Id = id
+	}
+
+	if err := db.Upsert(ctx, res); err != nil {
+		return nil, err
+	}
+	return &relay.UpdateStatusResponse{
+		Item: res.Status,
+	}, nil
+}
+
+func (s StatusService) getCurrentStatusID(ctx context.Context, db store.Store,
+	refType, refID string) (string,
+	error) {
+	currentStatus := resource.NewStatus()
+	err := db.Read(ctx, currentStatus,
+		store.GetByIndex("ctx_ref", model.MultiValueIndex(refType, refID)),
+	)
+	switch err {
+	case nil:
+		return currentStatus.Status.Id, nil
+	default:
+		if errors.Is(err, store.ErrNotFound) {
+			return "", nil
+		}
+		return "", err
+	}
+}
+
+func (s StatusService) ClearStatus(ctx context.Context,
+	req *relay.ClearStatusRequest) (*relay.ClearStatusResponse, error) {
+	err := validateRef(req.ContextReference)
+	if err != nil {
+		return nil, util.ErrClient{Message: err.Error()}
+	}
+	typ := req.ContextReference.Type
+	id := req.ContextReference.Id
+	s.logger.
+		With(zap.String("type", typ), zap.String("id", id)).
+		Debug("clear status invoked")
+
+	db, err := s.getDB(ctx, req.Cluster)
+	if err != nil {
+		return nil, err
+	}
+	statusID, err := s.getCurrentStatusID(ctx, db, typ, id)
+	if err != nil {
+		return nil, err
+	}
+	if id == "" {
+		return &relay.ClearStatusResponse{}, nil
+	}
+	err = db.Delete(ctx,
+		store.DeleteByType(resource.TypeStatus),
+		store.DeleteByID(statusID),
+	)
+	if err != nil {
+		if errors.Is(err, store.ErrNotFound) {
+			return &relay.ClearStatusResponse{}, nil
+		}
+		return nil, err
+	}
+	return &relay.ClearStatusResponse{}, nil
+}
+
+func (s StatusService) getDB(ctx context.Context,
+	cluster *adminModel.RequestCluster) (store.Store, error) {
+	store, err := s.storeLoader.Load(ctx, cluster)
+	if err != nil {
+		if storeLoadErr, ok := err.(util.StoreLoadErr); ok {
+			return nil, status.Error(storeLoadErr.Code, storeLoadErr.Message)
+		}
+		return nil, err
+	}
+	return store, nil
+}
+
+func validateRef(ref *adminModel.EntityReference) error {
+	if ref == nil {
+		return fmt.Errorf("no context reference")
+	}
+	id := ref.Id
+	if _, err := uuid.Parse(id); err != nil {
+		return fmt.Errorf("invalid id")
+	}
+	typ := model.Type(ref.Type)
+	if _, err := model.NewObject(typ); err != nil {
+		return fmt.Errorf("invalid type")
+	}
+	return nil
+}
---
+++ internal/server/relay/status_service_test.go
@@ -0,0 +1,285 @@
+package relay
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/uuid"
+	model "github.com/kong/koko/internal/gen/grpc/kong/admin/model/v1"
+	relay "github.com/kong/koko/internal/gen/grpc/kong/relay/service/v1"
+	"github.com/kong/koko/internal/log"
+	"github.com/kong/koko/internal/resource"
+	serverUtil "github.com/kong/koko/internal/server/util"
+	"github.com/kong/koko/internal/status"
+	"github.com/kong/koko/internal/store"
+	"github.com/kong/koko/internal/test/util"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+)
+
+func TestRelayStatusServiceUpdate(t *testing.T) {
+	ctx := context.Background()
+	persister, err := util.GetPersister()
+	require.Nil(t, err)
+	db := store.New(persister, log.Logger).ForCluster("default")
+	opts := StatusServiceOpts{
+		StoreLoader: serverUtil.DefaultStoreLoader{Store: db},
+		Logger:      log.Logger,
+	}
+	server := NewStatusService(opts)
+	require.NotNil(t, server)
+	l := setup()
+	s := grpc.NewServer()
+	relay.RegisterStatusServiceServer(s, server)
+	cc := clientConn(t, l)
+	client := relay.NewStatusServiceClient(cc)
+	go func() {
+		_ = s.Serve(l)
+	}()
+	defer s.Stop()
+
+	t.Run("updates a given status", func(t *testing.T) {
+		defer func() {
+			util.CleanDB()
+		}()
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		_, err := client.UpdateStatus(ctx, &relay.UpdateStatusRequest{
+			Item: &model.Status{
+				ContextReference: &model.EntityReference{
+					Type: "node",
+					Id:   uuid.NewString(),
+				},
+				Conditions: []*model.Condition{
+					{
+						Code:     status.DPMissingPlugin,
+						Message:  "foo bar",
+						Severity: resource.SeverityError,
+					},
+				},
+			},
+		})
+		require.Nil(t, err)
+		list := resource.NewList(resource.TypeStatus)
+		err = db.List(ctx, list)
+		require.NoError(t, err)
+		require.Len(t, list.GetAll(), 1)
+		item := list.GetAll()[0]
+		status, ok := item.Resource().(*model.Status)
+		require.True(t, ok)
+		require.Equal(t, status.ContextReference.Type, "node")
+		require.Equal(t, status.Conditions[0].Message, "foo bar")
+	})
+	t.Run("update current upserts status for an existing reference", func(t *testing.T) {
+		defer func() {
+			util.CleanDB()
+		}()
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		refID := uuid.NewString()
+		_, err := client.UpdateStatus(ctx, &relay.UpdateStatusRequest{
+			Item: &model.Status{
+				ContextReference: &model.EntityReference{
+					Type: "node",
+					Id:   refID,
+				},
+				Conditions: []*model.Condition{
+					{
+						Code:     status.DPMissingPlugin,
+						Message:  "foo",
+						Severity: resource.SeverityError,
+					},
+				},
+			},
+		})
+		require.Nil(t, err)
+		list := resource.NewList(resource.TypeStatus)
+		err = db.List(ctx, list)
+		require.NoError(t, err)
+		require.Len(t, list.GetAll(), 1)
+		item := list.GetAll()[0]
+		currentStatus, ok := item.Resource().(*model.Status)
+		require.True(t, ok)
+		require.Equal(t, currentStatus.ContextReference.Type, "node")
+		require.Equal(t, currentStatus.ContextReference.Id, refID)
+		require.Equal(t, currentStatus.Conditions[0].Message, "foo")
+		currentStatusID := currentStatus.Id
+
+		_, err = client.UpdateStatus(ctx, &relay.UpdateStatusRequest{
+			Item: &model.Status{
+				ContextReference: &model.EntityReference{
+					Type: "node",
+					Id:   refID,
+				},
+				Conditions: []*model.Condition{
+					{
+						Code:     status.DPMissingPlugin,
+						Message:  "foobar",
+						Severity: resource.SeverityError,
+					},
+				},
+			},
+		})
+		require.Nil(t, err)
+		list = resource.NewList(resource.TypeStatus)
+		err = db.List(ctx, list)
+		require.NoError(t, err)
+		require.Len(t, list.GetAll(), 1)
+		item = list.GetAll()[0]
+		currentStatus, ok = item.Resource().(*model.Status)
+		require.True(t, ok)
+		require.Equalf(t, currentStatusID, currentStatus.Id,
+			"status id to not change across updated")
+		require.Equal(t, currentStatus.ContextReference.Type, "node")
+		require.Equal(t, currentStatus.ContextReference.Id, refID)
+		require.Equal(t, currentStatus.Conditions[0].Message, "foobar")
+	})
+	t.Run("update without a condition errors", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		_, err := client.UpdateStatus(ctx, &relay.UpdateStatusRequest{
+			Item: &model.Status{
+				ContextReference: &model.EntityReference{
+					Type: "node",
+					Id:   uuid.NewString(),
+				},
+			},
+		})
+		require.Error(t, err)
+	})
+	t.Run("update without a reference errors", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		_, err := client.UpdateStatus(ctx, &relay.UpdateStatusRequest{
+			Item: &model.Status{
+				Conditions: []*model.Condition{
+					{
+						Code:     status.DPMissingPlugin,
+						Message:  "node",
+						Severity: resource.SeverityError,
+					},
+				},
+			},
+		})
+		require.Error(t, err)
+	})
+	t.Run("updates fails with an invalid ref type", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		_, err := client.UpdateStatus(ctx, &relay.UpdateStatusRequest{
+			Item: &model.Status{
+				ContextReference: &model.EntityReference{
+					Type: "foo",
+					Id:   uuid.NewString(),
+				},
+				Conditions: []*model.Condition{
+					{
+						Code:     status.DPMissingPlugin,
+						Message:  "foo bar",
+						Severity: resource.SeverityError,
+					},
+				},
+			},
+		})
+		require.Error(t, err)
+	})
+	t.Run("updates fails with an invalid ref id", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		_, err := client.UpdateStatus(ctx, &relay.UpdateStatusRequest{
+			Item: &model.Status{
+				ContextReference: &model.EntityReference{
+					Type: "node",
+					Id:   "borked-on-purpose",
+				},
+				Conditions: []*model.Condition{
+					{
+						Code:     status.DPMissingPlugin,
+						Message:  "foo bar",
+						Severity: resource.SeverityError,
+					},
+				},
+			},
+		})
+		require.Error(t, err)
+	})
+}
+
+func TestRelayStatusServiceClear(t *testing.T) {
+	ctx := context.Background()
+	persister, err := util.GetPersister()
+	require.Nil(t, err)
+	db := store.New(persister, log.Logger).ForCluster("default")
+	opts := StatusServiceOpts{
+		StoreLoader: serverUtil.DefaultStoreLoader{Store: db},
+		Logger:      log.Logger,
+	}
+	server := NewStatusService(opts)
+	require.NotNil(t, server)
+	l := setup()
+	s := grpc.NewServer()
+	relay.RegisterStatusServiceServer(s, server)
+	cc := clientConn(t, l)
+	client := relay.NewStatusServiceClient(cc)
+	go func() {
+		_ = s.Serve(l)
+	}()
+	defer s.Stop()
+
+	t.Run("clears a status", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+
+		refID := uuid.NewString()
+		_, err = client.UpdateStatus(ctx, &relay.UpdateStatusRequest{
+			Item: &model.Status{
+				ContextReference: &model.EntityReference{
+					Type: "node",
+					Id:   refID,
+				},
+				Conditions: []*model.Condition{
+					{
+						Code:     status.DPMissingPlugin,
+						Message:  "foo bar",
+						Severity: resource.SeverityError,
+					},
+				},
+			},
+		})
+
+		_, err = client.ClearStatus(ctx, &relay.ClearStatusRequest{
+			ContextReference: &model.EntityReference{
+				Type: "node",
+				Id:   refID,
+			},
+		})
+		require.NoError(t, err)
+
+		list := resource.NewList(resource.TypeStatus)
+		err = db.List(ctx, list)
+		require.NoError(t, err)
+		require.Len(t, list.GetAll(), 0)
+	})
+	t.Run("clear status throws an error with invalid type", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		_, err := client.ClearStatus(ctx, &relay.ClearStatusRequest{
+			ContextReference: &model.EntityReference{
+				Type: "borked",
+				Id:   uuid.NewString(),
+			},
+		})
+		require.Error(t, err)
+	})
+	t.Run("clear status throws an error with invalid id", func(t *testing.T) {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		_, err := client.ClearStatus(ctx, &relay.ClearStatusRequest{
+			ContextReference: &model.EntityReference{
+				Type: "node",
+				Id:   "borked",
+			},
+		})
+		require.Error(t, err)
+	})
+}
