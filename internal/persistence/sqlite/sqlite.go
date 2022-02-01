ba9dc56b8dc5669ed90745121b726200fd78e58e
rajkong <rajan.mahadevan@konghq.com>
2022-02-01 15:22:53 -0800

    First working implementation of sqlite3

--- internal/persistence/sqlite/sqlite.go
+++ internal/persistence/sqlite/sqlite.go
@@ -19,13 +19,13 @@ var listQuery = func(prefix string) string {
 	return fmt.Sprintf(`SELECT * from store where key glob '%s*'`, prefix)
 }
 
-type SQLite struct {
-	db *sql.DB
+var listQueryPaging = func(prefix string, limit int, offset int) string {
+	return fmt.Sprintf(`SELECT * FROM store where key glob '%s*' order by key limit %d offset %d;`,
+		prefix, limit, offset)
 }
 
-func (s *SQLite) ListWithPaging(ctx context.Context, prefix string, limit int, offset int) ([][2][]byte, error) {
-	//TODO implement me
-	panic("implement me")
+type SQLite struct {
+	db *sql.DB
 }
 
 type Opts struct {
@@ -121,6 +121,16 @@ func (s *SQLite) List(ctx context.Context, prefix string) ([][2][]byte, error) {
 	return res, err
 }
 
+func (s *SQLite) ListWithPaging(ctx context.Context, prefix string, limit int, offset int) ([][2][]byte, error) {
+	var res [][2][]byte
+	err := s.withinTx(ctx, func(tx persistence.Tx) error {
+		var err error
+		res, err = tx.ListWithPaging(ctx, prefix, limit, offset)
+		return err
+	})
+	return res, err
+}
+
 func (s *SQLite) Tx(ctx context.Context) (persistence.Tx, error) {
 	tx, err := s.db.BeginTx(ctx, nil)
 	if err != nil {
@@ -131,11 +141,6 @@ func (s *SQLite) Tx(ctx context.Context) (persistence.Tx, error) {
 
 type sqliteTx struct {
 	tx *sql.Tx
-}
-
-func (t *sqliteTx) ListWithPaging(ctx context.Context, prefix string, limit int, offset int) ([][2][]byte, error) {
-	//TODO implement me
-	panic("implement me")
 }
 
 func (t *sqliteTx) Commit() error {
@@ -197,6 +202,33 @@ func (t *sqliteTx) List(ctx context.Context, prefix string) ([][2][]byte,
 	error) {
 	var res [][2][]byte
 	rows, err := t.tx.QueryContext(ctx, listQuery(prefix))
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	if rows.Err() != nil {
+		return nil, rows.Err()
+	}
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		var (
+			resKey []byte
+			value  []byte
+		)
+		err := rows.Scan(&resKey, &value)
+		if err != nil {
+			return nil, err
+		}
+		res = append(res, [2][]byte{resKey, value})
+	}
+	return res, nil
+}
+
+func (t *sqliteTx) ListWithPaging(ctx context.Context, prefix string, limit int, offset int) ([][2][]byte, error) {
+	var res [][2][]byte
+	rows, err := t.tx.QueryContext(ctx, listQueryPaging(prefix, limit, offset))
 	if err != nil {
 		return nil, err
 	}
