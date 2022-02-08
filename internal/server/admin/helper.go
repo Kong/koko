534a4172cd9eb3570544d99c2a41d4dcb2fc35b5
rajkong <rajan.mahadevan@konghq.com>
2022-02-08 09:09:28 -0800

    Changing pagination result to total-count and removing offset

--- internal/server/admin/helper.go
+++ internal/server/admin/helper.go
@@ -1,9 +1,7 @@
 package admin
 
 import (
-	"encoding/base64"
 	"fmt"
-	"strconv"
 
 	pbModel "github.com/kong/koko/internal/gen/grpc/kong/admin/model/v1"
 	"github.com/kong/koko/internal/persistence"
@@ -36,17 +34,9 @@ func listOptsFromReq(listOpts *pbModel.Pagination) ([]store.ListOptsFunc, error)
 	return listOptFns, nil
 }
 
-func getOffset(totalCount int) string {
-	if totalCount == 0 {
-		return ""
-	}
-	// Converting to string first offset may not be int
-	return base64.StdEncoding.EncodeToString([]byte(strconv.Itoa(totalCount)))
-}
-
 func getPagination(totalCount int) *pbModel.PaginationResponse {
 	if totalCount == 0 {
 		return nil
 	}
-	return &pbModel.PaginationResponse{Offset: getOffset(totalCount)}
+	return &pbModel.PaginationResponse{TotalCount: fmt.Sprintf("%d", totalCount)}
 }
