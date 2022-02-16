9bd0f8e73f012c774bc8b0a4f9308d4ef903a9ff
Harry Bagdi <harrybagdi@gmail.com>
2022-02-16 07:14:51 -0800

    refactor: remove the old configuration builder

--- internal/server/kong/ws/config/config.go
+++ internal/server/kong/ws/config/config.go
@@ -1,52 +1,14 @@
 package config
 
 import (
-	"bytes"
-	"compress/gzip"
-	"encoding/json"
-	"fmt"
 	"sync"
 )
 
 type Map map[string]interface{}
 
-type Content struct {
-	FormatVersion string `json:"_format_version"`
-	Services      []Map  `json:"services,omitempty"`
-	Routes        []Map  `json:"routes,omitempty"`
-	Plugins       []Map  `json:"plugins,omitempty"`
-	Upstreams     []Map  `json:"upstreams,omitempty"`
-	Targets       []Map  `json:"targets,omitempty"`
-}
-
 type Payload struct {
 	compressed []byte
 	mu         sync.RWMutex
-}
-
-func (p *Payload) Update(config Content) error {
-	p.mu.Lock()
-	defer p.mu.Unlock()
-	payload := map[string]interface{}{
-		"type":         "reconfigure",
-		"config_table": config,
-	}
-	jsonMessage, err := json.Marshal(payload)
-	if err != nil {
-		return fmt.Errorf("json marshal: %v", err)
-	}
-	var buf bytes.Buffer
-	w := gzip.NewWriter(&buf)
-	_, err = w.Write(jsonMessage)
-	if err != nil {
-		return err
-	}
-	err = w.Close()
-	if err != nil {
-		return err
-	}
-	p.compressed = buf.Bytes()
-	return nil
 }
 
 func (p *Payload) Payload() []byte {
