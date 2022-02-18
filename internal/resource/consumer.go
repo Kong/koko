cf8d2f80cef292075708812466f8236b0caef028
rajkong <94076617+rajkong@users.noreply.github.com>
2022-02-18 03:13:46 -0800

    feat: and protobuf and model.Object for consumer entity (#18)

---
+++ internal/resource/consumer.go
@@ -0,0 +1,113 @@
+package resource
+
+import (
+	"fmt"
+
+	v1 "github.com/kong/koko/internal/gen/grpc/kong/admin/model/v1"
+	"github.com/kong/koko/internal/model"
+	"github.com/kong/koko/internal/model/json/generator"
+	"github.com/kong/koko/internal/model/json/validation"
+	"github.com/kong/koko/internal/model/json/validation/typedefs"
+)
+
+const (
+	TypeConsumer = model.Type("consumer")
+)
+
+func NewConsumer() Consumer {
+	return Consumer{
+		Consumer: &v1.Consumer{},
+	}
+}
+
+type Consumer struct {
+	Consumer *v1.Consumer
+}
+
+func (c Consumer) ID() string {
+	if c.Consumer == nil {
+		return ""
+	}
+	return c.Consumer.Id
+}
+
+func (c Consumer) Type() model.Type {
+	return TypeConsumer
+}
+
+func (c Consumer) Validate() error {
+	return validation.Validate(string(TypeConsumer), c.Consumer)
+}
+
+func (c Consumer) ProcessDefaults() error {
+	if c.Consumer == nil {
+		return fmt.Errorf("invalid nil resource")
+	}
+	defaultID(&c.Consumer.Id)
+	return nil
+}
+
+func (c Consumer) Resource() model.Resource {
+	return c.Consumer
+}
+
+func (c Consumer) Indexes() []model.Index {
+	maxIdxSize := 2
+	idx := make([]model.Index, 0, maxIdxSize)
+	if c.Consumer.Username != "" {
+		userIdx := model.Index{
+			Name:      "username",
+			Type:      model.IndexUnique,
+			Value:     c.Consumer.Username,
+			FieldName: "username",
+		}
+		idx = append(idx, userIdx)
+	}
+	if c.Consumer.CustomId != "" {
+		custIdx := model.Index{
+			Name:      "custom_id",
+			Type:      model.IndexUnique,
+			Value:     c.Consumer.CustomId,
+			FieldName: "custom_id",
+		}
+		idx = append(idx, custIdx)
+	}
+	return idx
+}
+
+func init() {
+	err := model.RegisterType(TypeConsumer, func() model.Object {
+		return NewConsumer()
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	consumerSchema := &generator.Schema{
+		Type: "object",
+		Properties: map[string]*generator.Schema{
+			"id":         typedefs.ID,
+			"username":   typedefs.Name,
+			"created_at": typedefs.UnixEpoch,
+			"updated_at": typedefs.UnixEpoch,
+			"custom_id":  typedefs.Name,
+			"tags":       typedefs.Tags,
+		},
+		AdditionalProperties: &falsy,
+		Required:             []string{"id"},
+		AnyOf: []*generator.Schema{
+			{
+				Description: "at-least one of custom_id or username must be set",
+				Required:    []string{"custom_id"},
+			},
+			{
+				Description: "at-least one of custom_id or username must be set",
+				Required:    []string{"username"},
+			},
+		},
+	}
+	err = generator.Register(string(TypeConsumer), consumerSchema)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ internal/resource/consumer_test.go
@@ -0,0 +1,93 @@
+package resource
+
+import (
+	"testing"
+
+	model "github.com/kong/koko/internal/gen/grpc/kong/admin/model/v1"
+	"github.com/kong/koko/internal/model/json/validation"
+	"github.com/stretchr/testify/require"
+)
+
+func TestConsumer_ID(t *testing.T) {
+	var c Consumer
+	id := c.ID()
+	require.Empty(t, id)
+	c = NewConsumer()
+	id = c.ID()
+	require.Empty(t, id)
+}
+
+func TestConsumer_Type(t *testing.T) {
+	require.Equal(t, TypeConsumer, NewConsumer().Type())
+}
+
+func TestConsumer_Defaults(t *testing.T) {
+	c := NewConsumer()
+	err := c.ProcessDefaults()
+	require.Nil(t, err)
+	require.True(t, validUUID(c.ID()))
+}
+
+func goodConsumer() Consumer {
+	c := NewConsumer()
+	_ = c.ProcessDefaults()
+	c.Consumer.Username = "my-company"
+	c.Consumer.CustomId = "my-company-ID"
+	return c
+}
+
+func TestConsumer_Validate(t *testing.T) {
+	t.Run("empty consumer must fail", func(t *testing.T) {
+		c := NewConsumer()
+		err := c.Validate()
+		require.Error(t, err)
+		verr, ok := err.(validation.Error)
+		require.True(t, ok)
+		e := []*model.ErrorDetail{
+			{
+				Type: model.ErrorType_ERROR_TYPE_ENTITY,
+				Messages: []string{
+					"missing properties: 'id'",
+					"at-least one of custom_id or username must be set",
+				},
+			},
+		}
+		require.ElementsMatch(t, verr.Errs, e)
+	})
+	t.Run("default consumer must fail", func(t *testing.T) {
+		c := NewConsumer()
+		_ = c.ProcessDefaults()
+		err := c.Validate()
+		require.Error(t, err)
+		verr, ok := err.(validation.Error)
+		require.True(t, ok)
+		e := []*model.ErrorDetail{
+			{
+				Type: model.ErrorType_ERROR_TYPE_ENTITY,
+				Messages: []string{
+					"at-least one of custom_id or username must be set",
+				},
+			},
+		}
+		require.ElementsMatch(t, verr.Errs, e)
+	})
+	t.Run("good consumer with username and no custom_id must pass", func(t *testing.T) {
+		c := NewConsumer()
+		_ = c.ProcessDefaults()
+		c.Consumer.Username = "my-company-name"
+		err := c.Validate()
+		require.NoError(t, err)
+	})
+	t.Run("good consumer with custom_id and no username must pass", func(t *testing.T) {
+		c := NewConsumer()
+		_ = c.ProcessDefaults()
+		c.Consumer.CustomId = "my-company-ID"
+		err := c.Validate()
+		require.NoError(t, err)
+	})
+	t.Run("good consumer with custom_id and username must pass", func(t *testing.T) {
+		c := goodConsumer()
+		err := c.Validate()
+		require.NoError(t, err)
+	})
+}
