21ea8dca0d64e47b3cb869543de05e0e107ba68c
Cecil Moore <cecil.moore@konghq.com>
2022-02-21 19:34:17 -0700

    add gin index on tags field of jsonb 'value' column

--- internal/db/sql/postgres/0001_init.up.sql
+++ internal/db/sql/postgres/0001_init.up.sql
@@ -1,1 +1,3 @@
+create extension if not exists pg_trgm; 
 create table if not exists store(key text PRIMARY KEY, value jsonb);
+create index if not exists store_tags_idx ON store USING gin ((value->'tags') jsonb_path_ops);
