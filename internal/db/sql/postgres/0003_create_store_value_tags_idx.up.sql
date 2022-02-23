801fbc9339ceab5d93289e1c640127a5a2546117
Cecil Moore <cecil.moore@konghq.com>
2022-02-23 12:34:02 -0700

    index tags nested under object

--- internal/db/sql/postgres/0003_create_store_value_tags_idx.up.sql
+++ internal/db/sql/postgres/0003_create_store_value_tags_idx.up.sql
@@ -1,1 +1,1 @@
-create index if not exists store_value_tags_idx on store USING gin ((value->'tags') jsonb_path_ops);
+create index if not exists store_value_tags_idx on store USING gin ((value->'object'->'tags') jsonb_path_ops);
