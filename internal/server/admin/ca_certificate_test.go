f1c1ef531363e87fcf1f75ee20fabedfbcb6921a
Gabriele Gerbino <gabrielegerbino@gmail.com>
2022-03-01 09:05:47 +0100

    feat: add CA certs CRUD API

---
+++ internal/server/admin/ca_certificate_test.go
@@ -0,0 +1,380 @@
+package admin
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/gavv/httpexpect/v2"
+	"github.com/google/uuid"
+	v1 "github.com/kong/koko/internal/gen/grpc/kong/admin/model/v1"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	caCertTemplate = x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			Organization: []string{"kong_clustering"},
+		},
+		NotBefore: time.Now(),
+		NotAfter:  time.Now().Add(time.Hour * 24),
+
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+
+	goodCertOne = `
+-----BEGIN CERTIFICATE-----
+MIICtjCCAZ6gAwIBAgIJAMajhTkQI3TIMA0GCSqGSIb3DQEBCwUAMBAxDjAMBgNV
+BAMMBUhlbGxvMB4XDTIyMDIyNzE4MTAzMVoXDTIyMDMyOTE4MTAzMVowEDEOMAwG
+A1UEAwwFSGVsbG8wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDhIxLU
+6qkYN6/E8wTNagvx+1aui5EwO+ImIA+RZxgnJsDNsg8R1hpABiaYWSNMa5wUs0mc
+S9tR/6vaC9VrGdEQXZs94b2qwUqmYdfsHpnLOYqFiZsg3BYTEnua/OWtEhI4LbIL
+wsTf2tLT0fBZZn3aNj/dVlt+almPDN8GML9gEio647tFCC1qkHVRaZGxsDZC5IfD
+7EiODwp540+CVQXsGaMJQZT2IoNwN96Cyw9h0ayJK2vJNRavBAohGEC13hTbbx1F
+eA8cjExmRW31G4J6kz2V+YGlBpXKPNRXO75kd33/IHaKqb35rGcd3OLZRQkMmSoY
+VaLzIEHQF+8HZVB7AgMBAAGjEzARMA8GA1UdEwQIMAYBAf8CAQAwDQYJKoZIhvcN
+AQELBQADggEBADGmkpZXyUjwPy1kWQpmve71vRCgWgDDc5eyXOzmKZsjYfGBJG4W
+3RcbOrbOLvrffjkdt7K7OqHGCc9J4Jp+FG3UC47wkOa/A1NOhPuBzr+YkKPRHMH7
+L+9FLfTFdRnPWnZ4CtNW5kOxAlpnrQDTFZll0AkKWqB3MYGpg2ilhtO7txwKHpgX
+T2WuO2v5B6TFtibsaYY8uMn/OpouEun0Cbns+KF9TDcqDO95TZSkoTW0bamVQH2T
+OhbS5BCFZxy+rJ2BD+gwtWfu3+8t+kiQeXXWVC+0qZahm98LgVKdXyCMxtviGMhh
+dHmsRtc2obmt+51SGycNsRZKPrD1WulKwj8=
+-----END CERTIFICATE-----
+`
+	digestOne = "a239094c44503b6a75071a098d6ef2fdbf1009343f60bbdbb17f52701cd823b1"
+
+	goodCertTwo = `
+-----BEGIN CERTIFICATE-----
+MIICtjCCAZ6gAwIBAgIJAM5Fd3HKorRaMA0GCSqGSIb3DQEBCwUAMBAxDjAMBgNV
+BAMMBUhlbGxvMB4XDTIyMDMwMzA5NDkzOFoXDTIyMDQwMjA5NDkzOFowEDEOMAwG
+A1UEAwwFSGVsbG8wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDZxtUd
+K3kn6JFRCznlbacn9qc0/vYUfJM9CGH3nlLoonI/6Bc+4ZRGKXPW0RVFynWq0cdq
+kpgxX9dcnKLXLI2DHhzY1qykW/ieLSjG5twyU0d3SxRr4gYy29KXjW7sWUEUrMmn
+zW+ody2YX3d+ppkUJJsmqucH4S+hUeIC8gm6cLE7H/R2GKvy/Ny0vsCT1xVXCFh5
+ko9FT1AhSierbPx2Njr7dNaKyuqR2gmhC7Aq6xLwDVBeMPoUXsLM8eXBntIrrQLw
+5FkHJIidLav5UgCYFKal+9bbtYdK2tO5xA+tu7BsKC1u0zZtEcX2/CzNPNJRitAR
+DIZMipzHeUrjtCfbAgMBAAGjEzARMA8GA1UdEwQIMAYBAf8CAQAwDQYJKoZIhvcN
+AQELBQADggEBAG90erz5GhzpahTm9KC5hKkyZbyUxvKLMkQSSB2S7go6irA9TAIW
+DOmRaQoVHh+BxX4bwqdzKhy24IPg4oe3xvD8RXJAtlersdZt3Hypfcfw9y2E3Bsr
+CmwpdYRh7iO++FqYXqMYWsHjvaiXmyXyO+ZLCDH3QUFKJEHC7ivP9zTTjgDqvLxd
+52qJFHgU99zcQPYJy4r6OhrkYTyCsmhY5+lmpxz6AENqGk2mQCwike2LlRJRy8fB
+RZzSYCLlEcxMDULSa0e1i6cQBbARUdDAaGLGZoNxCXB9i+6L9l8XsZgoCJXdjI8O
+XKfF8+VaoZcdgcddAc/pG1AQdEZVMEmSRcA=
+-----END CERTIFICATE-----
+`
+	digestTwo = "bbf6b98dcb52a3fb38e915fe3f6ce7b06ce32f83d86e7fe9b1ec6d3c78b41a95"
+)
+
+func TestCACertificateCreate(t *testing.T) {
+	s, cleanup := setup(t)
+	defer cleanup()
+	c := httpexpect.New(t, s.URL)
+
+	t.Run("creates a valid certificate", func(t *testing.T) {
+		cert := createCACert(t, &caCertTemplate)
+		res := c.POST("/v1/ca-certificates").WithJSON(&v1.CACertificate{
+			Cert: string(cert),
+		}).Expect()
+		res.Status(http.StatusCreated)
+		res.Header("grpc-metadata-koko-status-code").Empty()
+		body := res.JSON().Path("$.item").Object()
+		body.Value("cert").String().Equal(string(cert))
+	})
+	t.Run("creates a valid certificate and check digest", func(t *testing.T) {
+		res := c.POST("/v1/ca-certificates").WithJSON(&v1.CACertificate{
+			Cert: goodCertOne,
+		}).Expect()
+		res.Status(http.StatusCreated)
+		res.Header("grpc-metadata-koko-status-code").Empty()
+		body := res.JSON().Path("$.item").Object()
+		body.Value("cert").String().Equal(goodCertOne)
+		body.Value("cert_digest").String().Equal(digestOne)
+	})
+	t.Run("recreates same certificate fails", func(t *testing.T) {
+		res := c.POST("/v1/ca-certificates").WithJSON(&v1.CACertificate{
+			Cert: goodCertOne,
+		}).Expect()
+		res.Status(http.StatusBadRequest)
+		body := res.JSON().Object()
+		body.ValueEqual("message", "data constraint error")
+		body.Value("details").Array().Length().Equal(1)
+		errRes := body.Value("details").Array().Element(0)
+		errRes.Object().ValueEqual("type",
+			v1.ErrorType_ERROR_TYPE_REFERENCE.String())
+		errRes.Object().ValueEqual("field", "cert_digest")
+		errRes.Object().ValueEqual("messages", []string{
+			"cert_digest (type: unique) constraint failed for " +
+				"value 'a239094c44503b6a75071a098d6ef2fdbf1009343f60bbdbb17f52701cd823b1': ",
+		})
+	})
+	t.Run("creating an invalid certificate fails", func(t *testing.T) {
+		res := c.POST("/v1/ca-certificates").WithJSON(&v1.CACertificate{
+			Cert: "a",
+		}).Expect()
+		res.Status(http.StatusBadRequest)
+		body := res.JSON().Object()
+		body.ValueEqual("message", "validation error")
+		body.Value("details").Array().Length().Equal(1)
+		errRes := body.Value("details").Array().Element(0)
+		errRes.Object().ValueEqual("type",
+			v1.ErrorType_ERROR_TYPE_FIELD.String())
+		errRes.Object().ValueEqual("field", "cert")
+		errRes.Object().ValueEqual("messages", []string{
+			"invalid certificate: invalid PEM-encoded value",
+		})
+	})
+	t.Run("creating an invalid CA certificate fails", func(t *testing.T) {
+		template := caCertTemplate
+		template.BasicConstraintsValid = false
+		cert := createCACert(t, &template)
+		res := c.POST("/v1/ca-certificates").WithJSON(&v1.CACertificate{
+			Cert: string(cert),
+		}).Expect()
+		res.Status(http.StatusBadRequest)
+		body := res.JSON().Object()
+		body.ValueEqual("message", "validation error")
+		body.Value("details").Array().Length().Equal(1)
+		errRes := body.Value("details").Array().Element(0)
+		errRes.Object().ValueEqual("type",
+			v1.ErrorType_ERROR_TYPE_FIELD.String())
+		errRes.Object().ValueEqual("field", "cert")
+		errRes.Object().ValueEqual("messages", []string{
+			`certificate does not appear to be a CA because` +
+				`it is missing the "CA" basic constraint`,
+		})
+	})
+	t.Run("creating an expired CA certificate fails", func(t *testing.T) {
+		template := caCertTemplate
+		template.NotAfter = time.Now().Add(-24 * time.Hour)
+		cert := createCACert(t, &template)
+		res := c.POST("/v1/ca-certificates").WithJSON(&v1.CACertificate{
+			Cert: string(cert),
+		}).Expect()
+		res.Status(http.StatusBadRequest)
+		body := res.JSON().Object()
+		body.ValueEqual("message", "validation error")
+		body.Value("details").Array().Length().Equal(1)
+		errRes := body.Value("details").Array().Element(0)
+		errRes.Object().ValueEqual("type",
+			v1.ErrorType_ERROR_TYPE_FIELD.String())
+		errRes.Object().ValueEqual("field", "cert")
+		errRes.Object().ValueEqual("messages", []string{
+			`certificate expired, "Not After" time is in the past`,
+		})
+	})
+	t.Run("creating multiple CA certificates fails", func(t *testing.T) {
+		cert := string(createCACert(t, &caCertTemplate)) + goodCertOne
+		res := c.POST("/v1/ca-certificates").WithJSON(&v1.CACertificate{
+			Cert: cert,
+		}).Expect()
+		res.Status(http.StatusBadRequest)
+		body := res.JSON().Object()
+		body.ValueEqual("message", "validation error")
+		body.Value("details").Array().Length().Equal(1)
+		errRes := body.Value("details").Array().Element(0)
+		errRes.Object().ValueEqual("type",
+			v1.ErrorType_ERROR_TYPE_FIELD.String())
+		errRes.Object().ValueEqual("field", "cert")
+		errRes.Object().ValueEqual("messages", []string{
+			"only one certificate must be present",
+		})
+	})
+}
+
+func TestCACertificateUpsert(t *testing.T) {
+	s, cleanup := setup(t)
+	defer cleanup()
+	c := httpexpect.New(t, s.URL)
+
+	t.Run("upserts a valid certificate", func(t *testing.T) {
+		id := uuid.NewString()
+		resource := &v1.CACertificate{
+			Cert: goodCertOne,
+		}
+		res := c.PUT("/v1/ca-certificates/{id}", id).WithJSON(resource).Expect()
+		res.Status(http.StatusOK)
+		body := res.JSON().Path("$.item").Object()
+		body.Value("id").String().Equal(id)
+		body.Value("cert").String().Equal(goodCertOne)
+		body.Value("cert_digest").String().Equal(digestOne)
+		resource.Id = body.Value("id").String().Raw()
+	})
+	t.Run("upserts a valid certificate ignore digest", func(t *testing.T) {
+		id := uuid.NewString()
+		resource := &v1.CACertificate{
+			Cert:       goodCertTwo,
+			CertDigest: "a",
+		}
+		res := c.PUT("/v1/ca-certificates/{id}", id).WithJSON(resource).Expect()
+		res.Status(http.StatusOK)
+		body := res.JSON().Path("$.item").Object()
+		body.Value("id").String().Equal(id)
+		body.Value("cert").String().Equal(goodCertTwo)
+		body.Value("cert_digest").String().Equal(digestTwo)
+		resource.Id = body.Value("id").String().Raw()
+	})
+	t.Run("upsert an existing certificate succeeds", func(t *testing.T) {
+		cert := createCACert(t, &caCertTemplate)
+		res := c.POST("/v1/ca-certificates").WithJSON(&v1.Certificate{
+			Cert: string(cert),
+		}).Expect()
+		res.Status(201)
+		body := res.JSON().Path("$.item").Object()
+		id := body.Value("id").String().Raw()
+
+		cert = createCACert(t, &caCertTemplate)
+		certUpdate := &v1.CACertificate{
+			Id:   id,
+			Cert: string(cert),
+		}
+		res = c.PUT("/v1/ca-certificates/{id}", id).WithJSON(certUpdate).Expect()
+		res.Status(http.StatusOK)
+		body = res.JSON().Path("$.item").Object()
+		body.Value("id").String().Equal(id)
+		body.Value("cert").String().Equal(string(cert))
+	})
+	t.Run("upsert certificate without id fails", func(t *testing.T) {
+		res := c.PUT("/v1/ca-certificates/").
+			WithJSON(&v1.CACertificate{}).
+			Expect()
+		res.Status(http.StatusBadRequest)
+		body := res.JSON().Object()
+		body.ValueEqual("message", " '' is not a valid uuid")
+	})
+	t.Run("upsert an invalid certificate fails", func(t *testing.T) {
+		res := c.PUT("/v1/ca-certificates/{id}", uuid.NewString()).WithJSON(&v1.CACertificate{
+			Cert: "a",
+		}).Expect()
+		res.Status(400)
+		body := res.JSON().Object()
+		body.ValueEqual("message", "validation error")
+		body.Value("details").Array().Length().Equal(1)
+		errRes := body.Value("details").Array().Element(0)
+		errRes.Object().ValueEqual("type",
+			v1.ErrorType_ERROR_TYPE_FIELD.String())
+		errRes.Object().ValueEqual("field", "cert")
+		errRes.Object().ValueEqual("messages", []string{
+			"invalid certificate: invalid PEM-encoded value",
+		})
+	})
+}
+
+func TestCACertificateRead(t *testing.T) {
+	s, cleanup := setup(t)
+	defer cleanup()
+	c := httpexpect.New(t, s.URL)
+	res := c.POST("/v1/ca-certificates").WithJSON(&v1.CACertificate{
+		Cert: goodCertOne,
+	}).Expect()
+	res.Status(http.StatusCreated)
+	id := res.JSON().Path("$.item.id").String().Raw()
+	t.Run("read with an empty id returns 400", func(t *testing.T) {
+		res := c.GET("/v1/ca-certificates/").Expect().Status(http.StatusBadRequest)
+		body := res.JSON().Object()
+		body.ValueEqual("message", "required ID is missing")
+	})
+	t.Run("reading a non-existent certificate returns 404", func(t *testing.T) {
+		c.GET("/v1/ca-certificates/{id}", uuid.NewString()).
+			Expect().Status(http.StatusNotFound)
+	})
+	t.Run("reading with an exisiting certificate by returns 200", func(t *testing.T) {
+		res := c.GET("/v1/ca-certificates/{id}", id).
+			Expect().Status(http.StatusOK)
+		body := res.JSON().Path("$.item").Object()
+		body.Value("id").Equal(id)
+		body.Value("cert").Equal(goodCertOne)
+		body.Value("cert_digest").Equal(digestOne)
+	})
+}
+
+func TestCACertificateDelete(t *testing.T) {
+	s, cleanup := setup(t)
+	defer cleanup()
+	c := httpexpect.New(t, s.URL)
+	cert := createCACert(t, &caCertTemplate)
+	res := c.POST("/v1/ca-certificates").WithJSON(&v1.CACertificate{
+		Cert: string(cert),
+	}).Expect().Status(http.StatusCreated)
+	id := res.JSON().Path("$.item.id").String().Raw()
+	t.Run("deleting a non-existent certificate returns 404", func(t *testing.T) {
+		dres := c.DELETE("/v1/ca-certificates/{id}", uuid.NewString()).Expect()
+		dres.Status(http.StatusNotFound)
+	})
+	t.Run("delete with an invalid id returns 400", func(t *testing.T) {
+		dres := c.DELETE("/v1/ca-certificates/").Expect()
+		dres.Status(http.StatusBadRequest)
+	})
+	t.Run("delete an existing certificate succeeds", func(t *testing.T) {
+		dres := c.DELETE("/v1/ca-certificates/{id}", id).Expect()
+		dres.Status(http.StatusNoContent)
+	})
+}
+
+func TestCACertificateList(t *testing.T) {
+	s, cleanup := setup(t)
+	defer cleanup()
+	c := httpexpect.New(t, s.URL)
+
+	ids := make([]string, 0, 4)
+	for i := 0; i < 4; i++ {
+		cert := createCACert(t, &caCertTemplate)
+		res := c.POST("/v1/ca-certificates").WithJSON(&v1.Certificate{
+			Cert: string(cert),
+		}).Expect().Status(http.StatusCreated)
+		ids = append(ids, res.JSON().Path("$.item.id").String().Raw())
+	}
+
+	t.Run("list returns multiple certificates", func(t *testing.T) {
+		body := c.GET("/v1/ca-certificates").Expect().Status(http.StatusOK).JSON().Object()
+		items := body.Value("items").Array()
+		items.Length().Equal(4)
+		gotIDs := make([]string, 0, 4)
+		for _, item := range items.Iter() {
+			gotIDs = append(gotIDs, item.Object().Value("id").String().Raw())
+		}
+		require.ElementsMatch(t, ids, gotIDs)
+	})
+	t.Run("list returns multiple certificates with paging", func(t *testing.T) {
+		body := c.GET("/v1/ca-certificates").
+			WithQuery("page.size", "2").
+			WithQuery("page.number", "1").
+			Expect().Status(http.StatusOK).JSON().Object()
+		items := body.Value("items").Array()
+		items.Length().Equal(2)
+		gotIDs := make([]string, 0, 4)
+		for _, item := range items.Iter() {
+			gotIDs = append(gotIDs, item.Object().Value("id").String().Raw())
+		}
+		body.Value("page").Object().Value("total_count").Number().Equal(4)
+		next := body.Value("page").Object().Value("next_page_num").Number().Equal(2).Raw()
+		body = c.GET("/v1/ca-certificates").
+			WithQuery("page.size", "2").
+			WithQuery("page.number", next).
+			Expect().Status(http.StatusOK).JSON().Object()
+		body.Value("page").Object().Value("total_count").Number().Equal(4)
+		body.Value("page").Object().NotContainsKey("next_page")
+		items = body.Value("items").Array()
+		items.Length().Equal(2)
+		for _, item := range items.Iter() {
+			gotIDs = append(gotIDs, item.Object().Value("id").String().Raw())
+		}
+		require.ElementsMatch(t, ids, gotIDs)
+	})
+}
+
+func createCACert(t *testing.T, template *x509.Certificate) (certPEM []byte) {
+	caPrivKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	require.Nil(t, err)
+	der, err := x509.CreateCertificate(rand.Reader, template, template, &caPrivKey.PublicKey, caPrivKey)
+	require.Nil(t, err)
+	return pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: der})
+}
