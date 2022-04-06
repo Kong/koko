8b083b70a3397a4df4520695ae0eaa173b8ca4b2
Fero <6863207+mikefero@users.noreply.github.com>
2022-04-06 17:51:12 -0400

    perf: cache payload based on data plane version (#151)
    This is a performance enhancement for when multiple data planes are
    connected to a control plane; this will short circuit the processing of
    the payload.

--- internal/server/kong/ws/config/config.go
+++ internal/server/kong/ws/config/config.go
@@ -12,8 +12,19 @@ type Content struct {
 	Hash              string
 }
 
+// CachedContent holds the processed payload for a particular data plane
+// version. This content may be different than the actual Conent container as
+// the CompressedPayload could be updated for version compatibility.
+type CachedContent struct {
+	CompressedPayload []byte
+	// Error is stored to return the original error when accessing the cache
+	Error error
+	Hash  string
+}
+
 type Payload struct {
 	content Content
+	cache   map[string]CachedContent
 	mu      sync.RWMutex
 	vc      VersionCompatibility
 }
@@ -34,17 +45,23 @@ func NewPayload(opts PayloadOpts) (*Payload, error) {
 
 func (p *Payload) Payload(versionStr string) (Content, error) {
 	p.mu.RLock()
-	content := p.content
-	p.mu.RUnlock()
+	defer p.mu.RUnlock()
 
-	// TODO(fero): perf create cache; version aware
-	updatedPayload, err := p.vc.ProcessConfigTableUpdates(versionStr, content.CompressedPayload)
-	if err != nil {
-		return Content{}, fmt.Errorf("downgrade config: %v", err)
+	if _, found := p.cache[versionStr]; !found {
+		updatedPayload, err := p.vc.ProcessConfigTableUpdates(versionStr, p.content.CompressedPayload)
+		p.cache[versionStr] = CachedContent{
+			CompressedPayload: updatedPayload,
+			Error:             err,
+			Hash:              p.content.Hash,
+		}
+	}
+
+	if p.cache[versionStr].Error != nil {
+		return Content{}, fmt.Errorf("downgrade config: %v", p.cache[versionStr].Error)
 	}
 	return Content{
-		CompressedPayload: updatedPayload,
-		Hash:              content.Hash,
+		CompressedPayload: p.cache[versionStr].CompressedPayload,
+		Hash:              p.cache[versionStr].Hash,
 	}, nil
 }
 
@@ -52,5 +69,6 @@ func (p *Payload) UpdateBinary(c Content) error {
 	p.mu.Lock()
 	defer p.mu.Unlock()
 	p.content = c
+	p.cache = make(map[string]CachedContent)
 	return nil
 }
