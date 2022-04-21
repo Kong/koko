b532ffb9ca24199ceb3531263147d22eb78e1e7e
Cecil Moore <56893372+cecilmoore@users.noreply.github.com>
2022-04-21 09:50:46 -0600

    feat: use ISO8601 timestamp in logs
    From Kong/koko#175

--- internal/log/logger.go
+++ internal/log/logger.go
@@ -1,6 +1,11 @@
 package log
 
-import "go.uber.org/zap"
+import (
+	"fmt"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
 
 // Logger is setup on startup by cmd package.
 var Logger *zap.Logger
@@ -8,4 +13,21 @@ var Logger *zap.Logger
 func init() {
 	// initialized only for testing purposes.
 	Logger, _ = zap.NewDevelopment()
+}
+
+// SetupLogging configure parent logger with logLevel.
+func SetupLogging(logLevel string) (*zap.Logger, error) {
+	zapConfig := zap.NewProductionConfig()
+	zapConfig.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	level, err := zapcore.ParseLevel(logLevel)
+	if err != nil {
+		return nil, err
+	}
+	zapConfig.Level.SetLevel(level)
+	logger, err := zapConfig.Build()
+	if err != nil {
+		return nil, fmt.Errorf("create logger: %w", err)
+	}
+	Logger = logger
+	return logger, nil
 }
