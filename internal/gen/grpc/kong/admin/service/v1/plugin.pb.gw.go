8bfba3594900016ff20b3e00925327b26a47a589
Harry Bagdi <harrybagdi@gmail.com>
2022-05-06 13:26:38 -0700

    feat: introduce new /v1/configured-plugins endpoint
    The existing '/v1/configured_plugins' is semantically correct but uses
    an underscore in path definition, which is the only exception to the
    general unwritten rule of always using dashes for such purposes.
    The endpoint is considered deprecated and will be removed in a future
    release.

--- internal/gen/grpc/kong/admin/service/v1/plugin.pb.gw.go
+++ internal/gen/grpc/kong/admin/service/v1/plugin.pb.gw.go
@@ -67,6 +67,42 @@ func local_request_PluginService_GetConfiguredPlugins_0(ctx context.Context, mar
 
 }
 
+var (
+	filter_PluginService_GetConfiguredPlugins_1 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_PluginService_GetConfiguredPlugins_1(ctx context.Context, marshaler runtime.Marshaler, client PluginServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetConfiguredPluginsRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_PluginService_GetConfiguredPlugins_1); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetConfiguredPlugins(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_PluginService_GetConfiguredPlugins_1(ctx context.Context, marshaler runtime.Marshaler, server PluginServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetConfiguredPluginsRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_PluginService_GetConfiguredPlugins_1); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.GetConfiguredPlugins(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 func request_PluginService_GetAvailablePlugins_0(ctx context.Context, marshaler runtime.Marshaler, client PluginServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetAvailablePluginsRequest
 	var metadata runtime.ServerMetadata
@@ -429,6 +465,30 @@ func RegisterPluginServiceHandlerServer(ctx context.Context, mux *runtime.ServeM
 
 	})
 
+	mux.Handle("GET", pattern_PluginService_GetConfiguredPlugins_1, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		ctx, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/kong.admin.service.v1.PluginService/GetConfiguredPlugins", runtime.WithHTTPPathPattern("/v1/configured-plugins"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_PluginService_GetConfiguredPlugins_1(ctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_PluginService_GetConfiguredPlugins_1(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("GET", pattern_PluginService_GetAvailablePlugins_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -635,6 +695,27 @@ func RegisterPluginServiceHandlerClient(ctx context.Context, mux *runtime.ServeM
 
 	})
 
+	mux.Handle("GET", pattern_PluginService_GetConfiguredPlugins_1, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		ctx, err = runtime.AnnotateContext(ctx, mux, req, "/kong.admin.service.v1.PluginService/GetConfiguredPlugins", runtime.WithHTTPPathPattern("/v1/configured-plugins"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_PluginService_GetConfiguredPlugins_1(ctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_PluginService_GetConfiguredPlugins_1(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("GET", pattern_PluginService_GetAvailablePlugins_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -767,6 +848,8 @@ func RegisterPluginServiceHandlerClient(ctx context.Context, mux *runtime.ServeM
 var (
 	pattern_PluginService_GetConfiguredPlugins_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "configured_plugins"}, ""))
 
+	pattern_PluginService_GetConfiguredPlugins_1 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "configured-plugins"}, ""))
+
 	pattern_PluginService_GetAvailablePlugins_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "available-plugins"}, ""))
 
 	pattern_PluginService_GetPlugin_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "plugins", "id"}, ""))
@@ -782,6 +865,8 @@ var (
 
 var (
 	forward_PluginService_GetConfiguredPlugins_0 = runtime.ForwardResponseMessage
+
+	forward_PluginService_GetConfiguredPlugins_1 = runtime.ForwardResponseMessage
 
 	forward_PluginService_GetAvailablePlugins_0 = runtime.ForwardResponseMessage
 
