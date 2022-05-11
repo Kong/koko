162e7e8b76407cd9597a1439d95be8130237e7ee
Cecil Moore <cecil.moore@konghq.com>
2022-05-10 19:08:09 -0600

    move protojson specific code to proto_json.go file

--- internal/json/json.go
+++ internal/json/json.go
@@ -4,32 +4,6 @@ import (
 	"bytes"
 	stdjson "encoding/json"
 	"io"
-
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
-	"google.golang.org/protobuf/encoding/protojson"
-)
-
-var Marshaller runtime.Marshaler = &runtime.JSONPb{
-	MarshalOptions: protojson.MarshalOptions{
-		UseProtoNames: true,
-	},
-}
-
-// MarshallerWithDiscard discards unknown fields.
-// When in doubt, use Marshaller. This should be used very carefully and mostly
-// for temporary purposes.
-var MarshallerWithDiscard runtime.Marshaler = &runtime.JSONPb{
-	MarshalOptions: protojson.MarshalOptions{
-		UseProtoNames: true,
-	},
-	UnmarshalOptions: protojson.UnmarshalOptions{
-		DiscardUnknown: true,
-	},
-}
-
-var (
-	ProtoJSONMarshal   = Marshaller.Marshal
-	ProtoJSONUnmarshal = Marshaller.Unmarshal
 )
 
 // RawMessage is a raw encoded JSON value.
---
+++ internal/json/proto_json.go
@@ -0,0 +1,29 @@
+package json
+
+import (
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+var Marshaller runtime.Marshaler = &runtime.JSONPb{
+	MarshalOptions: protojson.MarshalOptions{
+		UseProtoNames: true,
+	},
+}
+
+// MarshallerWithDiscard discards unknown fields.
+// When in doubt, use Marshaller. This should be used very carefully and mostly
+// for temporary purposes.
+var MarshallerWithDiscard runtime.Marshaler = &runtime.JSONPb{
+	MarshalOptions: protojson.MarshalOptions{
+		UseProtoNames: true,
+	},
+	UnmarshalOptions: protojson.UnmarshalOptions{
+		DiscardUnknown: true,
+	},
+}
+
+var (
+	ProtoJSONMarshal   = Marshaller.Marshal
+	ProtoJSONUnmarshal = Marshaller.Unmarshal
+)
