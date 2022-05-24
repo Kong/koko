b2620d03c6f36b171733d5bd242f6d1d00e2d60e
Javier Guerra <javier.guerra@konghq.com>
2022-05-23 21:45:23 -0500

    config service: finish node initialization
    The first method in the config service is the `ReportMetadata`, which
    serves to finalize validation and initialization of the Node object.

--- internal/server/kong/ws/config_service.go
+++ internal/server/kong/ws/config_service.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/kong/go-wrpc/wrpc"
 	config_service "github.com/kong/koko/internal/gen/wrpc/kong/services/config/v1"
+	"go.uber.org/zap"
 )
 
 // Configer is the HandleRegisterer to be added to a Negotiator object.
@@ -37,6 +38,9 @@ func (c *Configer) PingCP(
 ) (resp *config_service.PingCPResponse, err error) {
 	// find out the Node
 	// update the reported hash
+	c.Manager.logger.Debug("received PingCP method",
+						   zap.String("nodeAddr", peer.RemoteAddr().String()),
+						   zap.String("hash", req.Hash))
 	node, ok := c.Manager.FindNode(peer.RemoteAddr().String())
 	if !ok {
 		return nil, fmt.Errorf("can't find node from %v", peer.RemoteAddr())
@@ -55,16 +59,34 @@ func (c *Configer) PingCP(
 }
 
 // Got the initial metadata (list of plugins)
-// validate versions and, if positive, only then
-// add the node to the manager.
+// then the manager can validate and promote the
+// node from "pending" to fully working.
 func (c *Configer) ReportMetadata(
 	ctx context.Context,
 	peer *wrpc.Peer,
 	req *config_service.ReportMetadataRequest,
 ) (resp *config_service.ReportMetadataResponse, err error) {
-	// find out the Node
-	// hope it's waiting for the metadata report
-	// push it there
+	c.Manager.logger.Debug("received ReportMetadata method",
+						   zap.String("nodeAddr", peer.RemoteAddr().String()))
+	node, ok := c.Manager.pendingNodes.FindNode(peer.RemoteAddr().String())
+	if !ok {
+		return nil, fmt.Errorf("can't find node from %v", peer.RemoteAddr())
+	}
+
+	var plugins []string
+	for _, p := range req.Plugins {
+		plugins = append(plugins, p.Name)
+	}
+	node.logger.Debug("plugin list", zap.Strings("plugins", plugins))
+
+	err = c.Manager.addWrpcNode(node, plugins) // nolint: contextcheck
+	if err != nil {
+		node.logger.Error("adding validated node")
+		node.Close()
+		return nil, err
+	}
+	node.logger.Debug("validated node added")
+
 	return &config_service.ReportMetadataResponse{
 		Response: &config_service.ReportMetadataResponse_Ok{
 			Ok: "valid",
