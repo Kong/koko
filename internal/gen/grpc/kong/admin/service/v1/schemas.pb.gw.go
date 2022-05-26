6c08e8c5b8cbd915ca4365282f202513023e0821
Gabriele Gerbino <gabrielegerbino@gmail.com>
2022-05-26 13:11:00 +0200

    feat: add cluster to GetLuaSchemasPlugin request

--- internal/gen/grpc/kong/admin/service/v1/schemas.pb.gw.go
+++ internal/gen/grpc/kong/admin/service/v1/schemas.pb.gw.go
@@ -423,6 +423,10 @@ func local_request_SchemasService_GetSchemas_0(ctx context.Context, marshaler ru
 
 }
 
+var (
+	filter_SchemasService_GetLuaSchemasPlugin_0 = &utilities.DoubleArray{Encoding: map[string]int{"name": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+)
+
 func request_SchemasService_GetLuaSchemasPlugin_0(ctx context.Context, marshaler runtime.Marshaler, client SchemasServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq GetLuaSchemasPluginRequest
 	var metadata runtime.ServerMetadata
@@ -442,6 +446,13 @@ func request_SchemasService_GetLuaSchemasPlugin_0(ctx context.Context, marshaler
 	protoReq.Name, err = runtime.String(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "name", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_SchemasService_GetLuaSchemasPlugin_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := client.GetLuaSchemasPlugin(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -468,6 +479,13 @@ func local_request_SchemasService_GetLuaSchemasPlugin_0(ctx context.Context, mar
 	protoReq.Name, err = runtime.String(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "name", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_SchemasService_GetLuaSchemasPlugin_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := server.GetLuaSchemasPlugin(ctx, &protoReq)
