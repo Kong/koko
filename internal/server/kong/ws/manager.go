Kong/koko

commit 4c8f56554e341d98fb413b9adce8690e042b897c

perf: avoid reconciliation of configuration on DP connect
This code predates the event-based refresh of configuration.
Refreshing configuration on a DP-connect was done to simplify
configuration refreshes. With the event-stream now, the configuration is
refreshed dynamically.

The stream is set up before a data-plane is tracked (via AddNode()).
This ensures that the payload will be reconcilied as required and a DP
connect event doesn't require a configuration refresh.

Furthermore, a large number of DP connects could cause a large number of
concurrent configuration reconciliation which is undesired and can even
result in an attack vector.

internal/server/kong/ws/manager.go
@@ -231,16 +231,6 @@ func (m *Manager) AddNode(node *Node) {
 				Error("remove node")
 		}
 	}()
-
-	// refresh the payload on DP connect
-	go func() {
-		err = m.reconcileKongPayload(context.Background())
-		if err != nil {
-			m.logger.With(zap.Error(err)).
-				Error("reconcile configuration")
-		}
-		m.broadcast()
-	}()
 }
 
 // broadcast sends the most recent configuration to all connected nodes.

commit f8712e3940b28fdf755e4f90a4c52eb199aee7b7

fix: push first configuration to node
If there are no update events after a node joins, it will not receive
any configuration at all.
This was a bug that was introduced in 4c8f56554.

This patch ensures that the manager pushes at least one configuration to
the DP.

internal/server/kong/ws/manager.go
@@ -231,6 +231,7 @@ func (m *Manager) AddNode(node *Node) {
 				Error("remove node")
 		}
 	}()
+	go m.broadcast()
 }
 
 // broadcast sends the most recent configuration to all connected nodes.