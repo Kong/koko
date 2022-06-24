0cf48025e2785c913b1a3df985f76c20fd87700b
Javier Guerra <javier.guerra@konghq.com>
2022-06-24 00:47:21 -0500

    rough adapting

--- internal/server/kong/ws/node.go
+++ internal/server/kong/ws/node.go
@@ -188,7 +188,7 @@ func (n *Node) sendConfig(ctx context.Context, payload *config.Payload) error {
 		return n.sendWrpcConfig(ctx, payload)
 	}
 
-	return fmt.Errorf("node unconnected")
+	return fmt.Errorf("node disconnected")
 }
 
 func (n *Node) sendJSONConfig(payload *config.Payload) error {
@@ -223,7 +223,7 @@ func (n *Node) sendWrpcConfig(ctx context.Context, payload *config.Payload) erro
 
 	hash, err := truncateHash(content.Hash)
 	if err != nil {
-		n.logger.With(zap.Error(err)).Sugar().Errorf("invalid hash [%v]", hash)
+		n.logger.With(zap.Error(err)).Sugar().Errorf("invalid hash [%s]", hash[:])
 		return err
 	}
 
