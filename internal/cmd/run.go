27d0885959b04bd6ca5839912d0515722d2433fe
Javier Guerra <javier.guerra@konghq.com>
2022-07-09 04:22:13 -0500

    Apply review suggestions

--- internal/cmd/run.go
+++ internal/cmd/run.go
@@ -287,9 +287,7 @@ func Run(ctx context.Context, config ServerConfig) error {
 			),
 		}
 	}
-	err = negotiator.AddService("config", "v1", "wRPC configuration", &ws.Configer{
-		Manager: m,
-	})
+	err = negotiator.AddService("config", "v1", "wRPC configuration", ws.NewConfigurer(m))
 	if err != nil {
 		return err
 	}
