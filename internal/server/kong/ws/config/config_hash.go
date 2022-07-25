07e338201974832e119cd4edb140beb010329117
Javier Guerra <javier.guerra@konghq.com>
2022-07-25 02:14:11 -0500

    add some comments

--- internal/server/kong/ws/config/config_hash.go
+++ internal/server/kong/ws/config/config_hash.go
@@ -9,6 +9,10 @@ import (
 
 const emptyHash = "00000000000000000000000000000000"
 
+// hashPart returns a hash for any Go object.
+// Result must be consistent for same values, no undefined ordering.
+// Must be 32 chars long.
+// (xxHash is only 16 hex chars, we duplicate it to match 32 chars.)
 func hashPart(v any) string {
 	h := xxhash.New()
 	e := json.NewEncoder(h)
@@ -19,6 +23,9 @@ func hashPart(v any) string {
 	return fmt.Sprintf("%x%x", sum, sum)
 }
 
+// getGranularHashes creates a hashing signature for a configuration object.
+// Returns a "total" value that represents the complete configuration,
+// as well as a map of separate hashes for each part of the configuration.
 func getGranularHashes(c DataPlaneConfig) (string, map[string]string) {
 	out := map[string]string{
 		"routes":    emptyHash,
