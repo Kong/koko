459fdfaf514449e80bfa38b4773b50485c2e286b
Taylor Jasko <taylor.jasko@konghq.com>
2022-08-17 13:42:01 -0500

    chore: remove unused `http-log` plugin headers rewrite logic
    Now that the `http-log` headers plugin migration (introduced in kong/koko#358)
    has been ran & executed, we no longer need this logic that handles rewriting
    the HTTP headers from an array to a single string.

--- internal/resource/plugin.go
+++ internal/resource/plugin.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	v1 "github.com/kong/koko/internal/gen/grpc/kong/admin/model/v1"
-	"github.com/kong/koko/internal/json"
 	"github.com/kong/koko/internal/model"
 	"github.com/kong/koko/internal/model/json/extension"
 	"github.com/kong/koko/internal/model/json/generator"
@@ -35,36 +34,6 @@ func NewPlugin() Plugin {
 
 type Plugin struct {
 	Plugin *v1.Plugin
-}
-
-// MarshalResourceJSON implements the ObjectWithResourceDTO.MarshalResourceJSON interface.
-func (r Plugin) MarshalResourceJSON() ([]byte, error) {
-	// This is the default behavior, but required to satisfy the ObjectWithResourceDTO interface.
-	return json.ProtoJSONMarshal(r.Plugin)
-}
-
-// UnmarshalResourceJSON implements the ObjectWithResourceDTO.UnmarshalResourceJSON interface.
-func (r Plugin) UnmarshalResourceJSON(b []byte) error {
-	if err := json.ProtoJSONUnmarshal(b, r.Plugin); err != nil {
-		return err
-	}
-
-	// In Kong 3.0, the `headers` config value was altered from an array of strings to a single
-	// string value. As such, we made a breaking change to update the underlining schema, which
-	// in turn requires API users to provide the headers as a single string.
-	//
-	// In order to migrate the headers within our DB from an array of strings to a single string
-	// value, we'll be supporting both ways in the interim, which will allow us to safely migrate
-	// the database without requiring code changes.
-	//
-	// TODO(tjasko): Once the DB migration has been completed, this re-write logic can be removed.
-	if r.Plugin.Name == pluginNameHTTPLog {
-		if err := rewriteHTTPLogHeadersFromListToString(r.Plugin); err != nil {
-			return err
-		}
-	}
-
-	return nil
 }
 
 func (r Plugin) ID() string {
