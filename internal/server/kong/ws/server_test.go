04d07087ddbfe3abc6b6d7134f065e8f5aafc796
Javier Guerra <javier.guerra@konghq.com>
2022-08-23 12:48:45 -0500

    apply review suggestions

--- internal/server/kong/ws/server_test.go
+++ internal/server/kong/ws/server_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestValidateRequest(t *testing.T) {
 	t.Run("no parameters, fail", func(t *testing.T) {
-		r, err := http.NewRequest("UPGRADE", "http://v1/outlet", nil)
+		r, err := http.NewRequest("UPGRADE", "http://example.com/v1/outlet", nil)
 		assert.NoError(t, err)
 
 		err = validateRequest(r)
@@ -19,7 +19,7 @@ func TestValidateRequest(t *testing.T) {
 
 	t.Run("accept sensible parameters", func(t *testing.T) {
 		r, err := http.NewRequest("UPGRADE",
-			"http://v1/outlet?node_id=000000&node_hostname=example.com&node_version=2.8.3", nil)
+			"http://example.com/v1/outlet?node_id=000000&node_hostname=example.com&node_version=2.8.3", nil)
 		assert.NoError(t, err)
 
 		err = validateRequest(r)
@@ -28,7 +28,7 @@ func TestValidateRequest(t *testing.T) {
 
 	t.Run("accept longer-than-semantic version", func(t *testing.T) {
 		r, err := http.NewRequest("UPGRADE",
-			"http://v1/outlet?node_id=000000&node_hostname=example.com&node_version=2.8.3.2-metapatch-edition", nil)
+			"http://example.com/v1/outlet?node_id=000000&node_hostname=example.com&node_version=2.8.3.2-metapatch-edition", nil)
 		assert.NoError(t, err)
 
 		err = validateRequest(r)
@@ -37,7 +37,7 @@ func TestValidateRequest(t *testing.T) {
 
 	t.Run("reject too old version", func(t *testing.T) {
 		r, err := http.NewRequest("UPGRADE",
-			"http://v1/outlet?node_id=000000&node_hostname=example.com&node_version=2.4.82", nil)
+			"http://example.com/v1/outlet?node_id=000000&node_hostname=example.com&node_version=2.4.82", nil)
 		assert.NoError(t, err)
 
 		err = validateRequest(r)
@@ -46,7 +46,7 @@ func TestValidateRequest(t *testing.T) {
 
 	t.Run("many builds does not a new version make", func(t *testing.T) {
 		r, err := http.NewRequest("UPGRADE",
-			"http://v1/outlet?node_id=000000&node_hostname=example.com&node_version=2.4.82.996-extra-strength", nil)
+			"http://example.com/v1/outlet?node_id=000000&node_hostname=example.com&node_version=2.4.82.996-extra-strength", nil)
 		assert.NoError(t, err)
 
 		err = validateRequest(r)
