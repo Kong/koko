b98ad7dfbabebf427f071342397d8a3c1ce0e995
Javier Guerra <javier.guerra@konghq.com>
2022-09-20 16:14:57 -0500

    apply review suggestions
     - remove ':latest' tag.
     - don't try to pull version tag. it must be provided.
     - allow setting vars via command args.

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -1,8 +1,34 @@
 #!/bin/bash -e
 
+while [ -n "$*" ]; do
+  case "$1" in
+    --user )
+      DOCKER_HUB_USERNAME="$2"
+      shift
+      ;;
+
+    --token )
+      DOCKER_HUB_ACCESS_TOKEN="$2"
+      shift
+      ;;
+
+    --tag )
+      GIT_TAG="$2"
+      shift
+      ;;
+  esac
+  shift
+done
+
 if [ -z "${DOCKER_HUB_USERNAME}" -o -z "${DOCKER_HUB_ACCESS_TOKEN}" ]; then
   echo "Missing Docker credentials."
   echo "Please set the variables DOCKER_HUB_USERNAME and DOCKER_HUB_ACCESS_TOKEN."
+  exit
+fi
+
+if [ -z "${GIT_TAG}" ]; then
+  echo "Missing version tag."
+  echo "Please set the GIT_TAG variable with the version name."
   exit
 fi
 
@@ -14,13 +40,6 @@ fi
 IMG_NAME="${IMG_NAME:-kong/koko}"
 GIT_COMMIT_HASH="${GIT_COMMIT_HASH:-$(git rev-parse --short HEAD)}"
 
-if [ -z "${GIT_TAG}" ]; then
-  GIT_TAG=$(git describe --tags --match 'v*' || true)
-fi
-if [ -z "${GIT_TAG}" ]; then
-  GIT_TAG="dev-${GIT_COMMIT_HASH}"
-fi
-
 set -v
 
 echo "${DOCKER_HUB_ACCESS_TOKEN}" |
@@ -29,6 +48,6 @@ echo "${DOCKER_HUB_ACCESS_TOKEN}" |
 docker buildx build \
   --build-arg "GIT_TAG=${GIT_TAG}" \
   --build-arg "GIT_COMMIT_HASH=${GIT_COMMIT_HASH}" \
-  -t "${IMG_NAME}:latest" -t "${IMG_NAME}:${GIT_TAG}" \
+  -t "${IMG_NAME}:${GIT_TAG}" \
   --push \
   .
