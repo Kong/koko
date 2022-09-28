2b01f9f8e8baaa1567d5d1ec4d243c9abcafcccf
Taylor Jasko <taylor.jasko@konghq.com>
2022-09-28 17:03:33 -0500

    tests: add MySQL to the test suite in CI

--- scripts/setup-db.sh
+++ scripts/setup-db.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-docker run \
-  -e POSTGRES_USER=koko \
-  -e POSTGRES_PASSWORD=koko \
-  -p 5432:5432 \
-  --name koko_postgres \
-  postgres
+if [ $# -eq 0 ]; then
+  echo "Missing argument to indicate DB dialect"
+  exit 1
+fi
+
+case $1 in
+  mysql)
+    docker run \
+      -e MYSQL_DATABASE=koko \
+      -e MYSQL_USER=koko \
+      -e MYSQL_PASSWORD=koko \
+      -e MYSQL_ALLOW_EMPTY_PASSWORD=yes \
+      -p 3306:3306 \
+      --name "koko_$1" \
+      mysql:8.0.17
+    ;;
+
+  postgres)
+    docker run \
+      -e POSTGRES_USER=koko \
+      -e POSTGRES_PASSWORD=koko \
+      -p 5432:5432 \
+      --name "koko_$1" \
+      postgres
+    ;;
+esac
--- scripts/wait-for-db.sh
+++ scripts/wait-for-db.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-for i in {1..120}; do
-  running=`docker inspect -f {{.State.Running}} koko_postgres 2>&1`
+if [ $# -eq 0 ]; then
+  echo "Missing argument to indicate DB dialect"
+  exit 1
+fi
+
+if [[ $1 == "sqlite3" ]]; then
+  exit 0
+fi
+
+for _ in {1..120}; do
+  running=$(docker inspect -f '{{.State.Running}}' "koko_$1" 2>&1)
   if [[ $running == "true" ]]; then
     echo 'db running'
     exit 0
