0a3e6e459648acabf9e92c50d55e9b7f35e9d18b
Taylor Jasko <taylor.jasko@konghq.com>
2022-09-28 16:48:35 -0500

    tests: add MySQL to the test suite in CI

--- scripts/setup-db.sh
+++ scripts/setup-db.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-docker run \
-  -e POSTGRES_USER=koko \
-  -e POSTGRES_PASSWORD=koko \
-  -p 5432:5432 \
-  --name koko_postgres \
-  postgres
+if [ $# -eq 0 ]; then
+  echo "Missing argument to indicate DB dialect"
+  exit 1
+fi
+
+case $1 in
+  mysql)
+    docker run \
+      -e MYSQL_ALLOW_EMPTY_PASSWORD=yes \
+      -p 3306:3306 \
+      --name "koko_$1" \
+      mysql:8.0.17
+    ;;
+
+  postgres)
+    docker run \
+      -e POSTGRES_USER=koko \
+      -e POSTGRES_PASSWORD=koko \
+      -p 5432:5432 \
+      --name "koko_$1" \
+      postgres
+    ;;
+esac
