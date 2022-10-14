af853084f3856978067ded3458f43a8cdb3830be
Alex Gaesser <alex.gaesser@konghq.com>
2022-10-14 13:25:43 -0700

    util smoke tests

---
+++ internal/metrics/v2/util_test.go
@@ -0,0 +1,38 @@
+package v2
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"reflect"
+	"testing"
+)
+
+func TestToPrometheusLabel(t *testing.T) {
+	tests := []struct {
+		name   string
+		labels []Label
+		want   prometheus.Labels
+	}{
+		{
+			name:   "should return a valid list of labels",
+			labels: []Label{{Key: "foo", Value: "bar"}},
+			want:   prometheus.Labels{"foo": "bar"},
+		},
+		{
+			name:   "should return an empty label list when input is empty",
+			labels: []Label{},
+			want:   prometheus.Labels{},
+		},
+		{
+			name:   "should return an empty label list when input is nil",
+			labels: nil,
+			want:   prometheus.Labels{},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if got := toPrometheusLabel(test.labels...); !reflect.DeepEqual(got, test.want) {
+				t.Errorf("toPrometheusLabel() = %v, want %v", got, test.want)
+			}
+		})
+	}
+}
